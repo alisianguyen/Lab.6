74c4baba6965c6a4d7efd4996b4006daab9a9754
ejcleary <ejcleary03@gmail.com>
2023-07-19 19:38:42 -0400

    Ethans Commit

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
 
+# Ethan Cleary
+def encode(password):
+    global enc_password
+    enc_password = ''
+    for num in password:
+        new_num = str(int(num) + 3)
+        enc_password = enc_password + new_num
+    return enc_password
 
-whatever hopefully this works
+
+
+
+while True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print("")
+
+    option = int(input("Please enter an option: "))
+
+    if option == 1:
+        password = input("Please enter your password to encode: ")
+        print("Your password has been encoded and stored!")
+        print("")
+    elif option == 2:
+        print(f"The encoded password is {encode(password)}, and the original password is {decode(password)}.")
+        print("")
+    elif option == 3:
+        break
+
+def encode(password):
+    global enc_password
+    enc_password = ''
+    for num in password:
+        new_num = str(int(num) + 3)
+        enc_password = enc_password + new_num
+    return enc_password
 
 
 
